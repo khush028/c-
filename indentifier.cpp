84bbf0f690fd39b0c341af010bee63bee96acae9
khushbu jaim <khushbujaink7568@gmail.com>
2024-07-16 01:30:42 +0530

    Add files via upload

---
+++ indentifier.cpp
@@ -0,0 +1,14 @@
+// indentifiers refers to the name that is used to identifie variable , function and so on 
+// not all indentifier are variables
+#include<iostream>
+using namespace std;
+void Myname() // myname is  identifier 
+{
+    cout<<" jain";
+}
+int main()
+{
+    string name ="khushbu";
+    cout<<name;
+    Myname();// we call the function
+}
