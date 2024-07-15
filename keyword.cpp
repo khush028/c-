ed2f33cf8ce9ffe179b4dab51cd9ea348f16483d
khushbu jaim <khushbujaink7568@gmail.com>
2024-07-16 01:30:14 +0530

    Add files via upload

---
+++ keyword.cpp
@@ -0,0 +1,12 @@
+// keyword = is resrved word whose meaning already defined on the complier
+//  example =  for  = 2;(not allowed)
+// we can't use keyword as a variable and constant name
+// keyword must bi in lowercase
+
+#include<iostream>
+using namespace std;
+main()
+{
+    //int for = 10;
+    //cout<<for;
+}
