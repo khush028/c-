9c04930ccf519887b63c4b2e873bab8342b9d85a
khushbu jaim <khushbujaink7568@gmail.com>
2024-07-16 15:48:15 +0530

    Add files via upload

---
+++ conditionalstatement.cpp
@@ -0,0 +1,30 @@
+/*conditional statement allow you to make a decision based on the result of condition
+types
+if statment
+if else
+if else if
+nested if 
+switch statment 
+*/
+
+// if statement = is used when we want to test a condition
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a ;
+    cout<<"enter you age";
+    cin>>a;
+
+    if(a >= 18)
+    {
+        cout<<"you can  vote";
+    }
+    if(a<18)
+    {
+        cout<<"you can't vote";
+    }
+    
+
+}
